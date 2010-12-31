824acec5f591ace4059d354d55104de668b66e6e
Doug Youch <doug@cykod.com>
2010-12-31 15:07:58 -0500

    Added timeout

--- app/models/tweet_data.rb
+++ app/models/tweet_data.rb
@@ -1,19 +1,22 @@
+require 'timeout'
 
 class TweetData
   def self.delayed_tweet_collector(args)
     begin
-      if args[:screen_name]
-        @data = Tweet::AdminController.module_options.twitter.user_timeline('screen_name' => args[:screen_name])
-      else
-        @data = Tweet::AdminController.module_options.twitter.user_timeline()
+      Timeout::timeout(10) do
+        if args[:screen_name]
+          @data = Tweet::AdminController.module_options.twitter.user_timeline('screen_name' => args[:screen_name])
+        else
+          @data = Tweet::AdminController.module_options.twitter.user_timeline()
+        end
+
+        return nil unless @data
+
+        @tweet_data = @data.map { |elm| dt = elm.to_hash; dt['user'] = dt['user'].to_hash; dt }
+
+        limit = args[:limit].to_i
+        @tweet_data = @tweet_data[0..(limit-1)] if limit > 0
       end
-
-      return nil unless @data
-
-      @tweet_data = @data.map { |elm| dt = elm.to_hash; dt['user'] = dt['user'].to_hash; dt }
-
-      limit = args[:limit].to_i
-      @tweet_data = @tweet_data[0..(limit-1)] if limit > 0
     rescue Exception => e
       return nil
     end
